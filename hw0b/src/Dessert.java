14554e02f6dde83219aa0755568d76b4e91ec057
Outre <3340931187@qq.com>
2024-07-10 17:01:30 +0800

    finish lab01

--- hw0b/src/Dessert.java
+++ hw0b/src/Dessert.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Dessert {
+public class Dessert {
+    private final int flavor;
+    private final int price;
+    public static int numDesserts;
+
+    public Dessert(int flavor, int price) {
+        this.flavor = flavor;
+        this.price = price;
+        numDesserts++;
+    }
+
+    public void printDessert() {
+        System.out.println(flavor + " " + price + " " + numDesserts);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("I love dessert!");
+    }
 }
